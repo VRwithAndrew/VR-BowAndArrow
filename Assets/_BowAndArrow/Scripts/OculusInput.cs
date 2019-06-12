7114ab2daccf12b1373ca1c89047a9097053e29a
Andrew <drew.l.connell@gmail.com>
2019-06-12 09:14:13 -0400

    Initial Project

---
+++ Assets/_BowAndArrow/Scripts/OculusInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using OVR;
+
+public class OculusInput : MonoBehaviour
+{
+    /*
+    public Bow m_Bow = null;
+    public GameObject m_OppositeController = null;
+    public OVRInput.Controller m_Controller = OVRInput.Controller.None;
+
+    private void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
+            m_Bow.Pull(m_OppositeController.transform);
+
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
+            m_Bow.Release();
+    }
+    */
+}
---
+++ Assets/_BowAndArrow/Scripts/SteamInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+// using Valve.VR;
+
+public class SteamInput : MonoBehaviour
+{
+    /*
+    public Bow m_Bow = null;
+    public SteamVR_Behaviour_Pose m_Pose = null;
+    public SteamVR_Action_Boolean m_PullAction = null;
+
+    private void Update()
+    {
+        if (m_PullAction.GetStateDown(m_Pose.inputSource))
+            m_Bow.Pull(m_Pose.gameObject.transform);
+
+        if (m_PullAction.GetStateUp(m_Pose.inputSource))
+            m_Bow.Release();
+    }
+    */
+}
